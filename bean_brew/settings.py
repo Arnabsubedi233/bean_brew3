406a1c1afef74b645ce0f5301d95ba957c7e4059
arnabsubedi22 <alexgamer1819@gmail.com>
2024-02-13 20:22:38 +0000

    added base html files and linked css

--- bean_brew/settings.py
+++ bean_brew/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bean_brew.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'webpages')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,13 +118,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'Static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'Staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'Media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 
--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to bean and brew")
+    return render(request,"home/homepage.html")
