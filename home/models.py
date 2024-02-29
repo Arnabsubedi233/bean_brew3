b442c31d666ddb174e5ed0bb13bab76b670c774e
arnabsubedi22 <alexgamer1819@gmail.com>
2024-02-29 21:01:46 +0000

    reservation model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class reservation(models.Model):
+
+    CITY =[
+        ('Harrogate','Harrogate'),
+        ('Leeds','Leeds'),
+        ('Knaresborough Castle','Knaresborough')
+    ]
+    name = models.CharField(max_length = 50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    restaurant = models.CharField(max_length = 100,choices=CITY)
+    number_persons = models.IntegerField()
+    date = models.DateField() 
+    time = models.TimeField
+
+    def __str__(self):
+        return self.name
